60a32ab3c55cfebe59023b11f181ee15c5c7e23d
Ganesh <Ganesh@192.168.0.104>
2016-03-11 00:05:39 +0530

    test commit

--- src/LibraryFunc/FirstClassTry.java
+++ src/LibraryFunc/FirstClassTry.java
@@ -3,8 +3,7 @@ package LibraryFunc;
 public class FirstClassTry {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+System.out.println("test me the aldsadas");
 	}
 
 }
